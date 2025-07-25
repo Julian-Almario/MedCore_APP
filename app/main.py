4b83dac5166ada6faeee995573722b31be19a5ef
Julian Almario <doctorz.0525@gmail.com>
2025-07-24 22:33:58 -0500

    algoritmos

--- app/main.py
+++ app/main.py
@@ -205,6 +205,101 @@ def main(page: ft.Page):
                 ),
             ],
         )
+#---------------------------------------------------
+    RUTA_ALGO = os.path.abspath(os.path.join(os.path.dirname(__file__), "storage", "data", "algoritmos"))
+    os.makedirs(RUTA_ALGO, exist_ok=True)
+
+    # Extensiones válidas de imagen
+    EXTENSIONES_VALIDAS = [".jpg", ".jpeg", ".png", ".webp", ".bmp", ".gif"]
+
+    def es_imagen_valida(ruta_imagen):
+        return os.path.exists(ruta_imagen) and os.path.splitext(ruta_imagen)[1].lower() in EXTENSIONES_VALIDAS
+
+    def cargar_algoritmos_desde_json(ruta_archivo):
+        with open(ruta_archivo, "r", encoding="utf-8") as f:
+            datos = json.load(f)
+        return [crear_panel_algoritmo(algo) for algo in datos]
+
+    def crear_panel_algoritmo(algo: dict):
+        panel_ref = ft.Ref[ft.ExpansionPanel]()
+
+        def on_expand_change(e):
+            panel = panel_ref.current
+            is_expanded = panel.expanded
+            panel.bgcolor = SECONDARY_COLOR if is_expanded else PRIMARY_COLOR
+            panel.update()
+
+        ruta_imagen = os.path.join(RUTA_ALGO, algo["imagen"])
+        
+        if not es_imagen_valida(ruta_imagen):
+            imagen_componente = ft.Text(
+                f"Imagen no encontrada o formato no soportado: {algo['imagen']}",
+                color=ft.Colors.RED,
+                selectable=True
+            )
+        else:
+            imagen_componente = ft.Image(
+                src=ruta_imagen,
+                fit=ft.ImageFit.CONTAIN,
+                expand=True,
+            )
+
+        return {
+            "titulo": algo["nombre"],
+            "tags": algo["tags"],
+            "componente": ft.ExpansionPanelList(
+                on_change=on_expand_change,
+                expand_icon_color=TEXT_COLOR,
+                elevation=8,
+                divider_color=TEXT_COLOR,
+                controls=[
+                    ft.ExpansionPanel(
+                        ref=panel_ref,
+                        header=ft.ListTile(
+                            title=ft.Text(algo["nombre"], text_align=ft.TextAlign.LEFT),
+                        ),
+                        content=ft.Container(
+                            content=imagen_componente,
+                            padding=15,
+                        ),
+                        bgcolor=PRIMARY_COLOR,
+                        expanded=False,
+                    )
+                ],
+            )
+        }
+
+    def pagina_algoritmos(page: ft.Page):
+        list_data = cargar_algoritmos_desde_json(os.path.join(RUTA_ALGO, "algoritmos.json"))
+        buscar = "Buscar algoritmos..."
+
+        mensaje_no_resultados = ft.Text(
+            value="No se encontraron algoritmos.",
+            style=ft.TextThemeStyle.BODY_MEDIUM,
+            text_align=ft.TextAlign.CENTER,
+            color=ft.Colors.ON_SURFACE_VARIANT,
+        )
+
+        list_container, filtrar_items = list_content_search(list_data, mensaje_no_resultados)
+
+        return ft.Column(
+            expand=True,
+            controls=[
+                ft.Container(
+                    content=search_bar(filtrar_items, buscar),
+                    padding=ft.padding.symmetric(horizontal=40, vertical=10),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    expand=True,
+                    content=ft.ListView(
+                        expand=True,
+                        padding=ft.padding.symmetric(horizontal=10, vertical=5),
+                        controls=[list_container],
+                    ),
+                ),
+            ],
+        )
 
 # -------------------------------------------------------------------------------
 
@@ -227,6 +322,12 @@ def main(page: ft.Page):
         main_content.controls.clear()
         main_content.controls.append(pantalla_historia_clinica(page))
         page.update()
+    
+    def show_algorithms():
+        main_content.controls.clear()
+        main_content.controls.append(pagina_algoritmos(page))
+        page.update()
+
 
     def show_info():
         main_content.controls.clear()
@@ -280,6 +381,13 @@ def main(page: ft.Page):
                     style=ft.ButtonStyle(color=TEXT_COLOR if current_page_index == 4 else SELECT_COLOR),
                     on_click=lambda e: cambiar_pagina(4),
                 ),
+                ft.TextButton(
+                    text="Algoritmos",
+                    icon=ft.Icons.DEVICE_HUB_OUTLINED,
+                    style=ft.ButtonStyle(color=TEXT_COLOR if current_page_index == 5 else SELECT_COLOR),
+                    on_click=lambda e: cambiar_pagina(5),
+                ),
+
             ]
         )
 
@@ -307,6 +415,9 @@ def main(page: ft.Page):
             show_hc()
         elif current_page_index == 4:
             show_info()
+        elif current_page_index == 5:
+            show_algorithms()
+
 
 
     load_current_page()
